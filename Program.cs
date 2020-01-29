0803104b99b9959f69c7959e2e98a16e71d2c4da
Matias Moreira <moreiramatias10@hotmail.com>
2020-01-29 04:28:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumasSucesivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, producto = 0;
+            Console.Write("Ingresar un valor: ");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingresar un valorpor el que desea multiplicar el anterior: ");
+            valor2 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < valor2; i++)
+            {
+                producto += valor1; 
+            }
+            Console.Write("El producto de ambos valores es: " + producto.ToString());
+        }
+    }
+}
